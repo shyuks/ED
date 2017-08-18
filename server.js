afc73feb62f717724dce45539d233dfc6404965d
eSsXiV <essxiv@gmail.com>
2017-08-17 21:24:51 -0700

    updated

--- server.js
+++ server.js
@@ -8,25 +8,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/2', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/index2.html'));
+app.get('/A', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/Index_A.html'))
 });
 
 app.get('/B', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/indexB.html'));
+    res.sendFile(path.join(__dirname, './public/Index_B.html'));
+});
+
+app.get('/C', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/Index_C.html'));
+});
+
+app.get('/D', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/Index_D.html'));
+});
+
+app.get('/E', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/Index_E.html'));
 });
 
 app.get('/search', function(req, res) {
     res.sendFile(path.join(__dirname, './public/templates/search.html'))
-})
+});
 
 app.get('/article', function(req, res) {
     res.sendFile(path.join(__dirname, './public/templates/article.html'))
-})
+});
 
 app.listen(3000, function() {
     console.log('Listening On 3000...')
