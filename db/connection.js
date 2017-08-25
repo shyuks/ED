042df966823ef9228f5e2c053fb1628185a2ba53
eSsXiV <essxiv@gmail.com>
2017-08-24 19:09:32 -0700

    email func

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongoURI = 'mongodb://138.68.248.193:27017/test'
+const mongoURI = 'mongodb://127.0.0.1:27017/test'
 mongoose.connect(mongoURI)
 
 const db = mongoose.connection
--- server.js
+++ server.js
@@ -2,12 +2,50 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var db = require('./db/connection.js');
+var nodemailer = require('nodemailer');
+
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/sendSubscription', function(req, res) {
+
+    if (typeof(req.body.payload) === 'string') {
+
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // secure:true for port 465, secure:false for port 587
+            auth: {
+                user: 'sangenyx@gmail.com',
+                pass: 'Ogabogaa8'
+            }
+        });
+
+        let mailOptions = {
+            to: req.body.payload,
+            from: 'sangenyx@gmail.com',
+            subject: 'Doctorpedia News Subscription',
+            text: 'Hello',
+            html: '<b>Welcome to Doctorpedia News!</b>'
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        });
+
+        res.send('sent email');
+        return;
+    } else {
+        res.send('error in sending email');
+    }
+})
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './public/404/routing.html'));
@@ -48,6 +86,7 @@ app.get('/article-video', function(req, res) {
 app.get('/contact-us', function(req, res) {
     res.sendFile(path.join(__dirname, './public/templates/contact_us.html'));
 });
+
 
 app.listen(3000, function() {
     console.log('Listening On http://138.68.248.193:8080/');
